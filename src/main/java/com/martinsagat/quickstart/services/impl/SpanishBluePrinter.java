fc462b33daf61b3941f97365cd0b20b57a07022a
Martin Sagat <martin.sagat@outlook.com.au>
2025-03-27 20:57:34 +0800

    implement spanish printers

---
+++ src/main/java/com/martinsagat/quickstart/services/impl/SpanishBluePrinter.java
@@ -0,0 +1,11 @@
+package com.martinsagat.quickstart.services.impl;
+
+import com.martinsagat.quickstart.services.BluePrinter;
+
+public class SpanishBluePrinter implements BluePrinter {
+
+  @Override
+  public String print() {
+    return "Azul";
+  }
+}
---
+++ src/main/java/com/martinsagat/quickstart/services/impl/SpanishGreenPrinter.java
@@ -0,0 +1,11 @@
+package com.martinsagat.quickstart.services.impl;
+
+import com.martinsagat.quickstart.services.GreenPrinter;
+
+public class SpanishGreenPrinter implements GreenPrinter {
+
+  @Override
+  public String print() {
+    return "Verde";
+  }
+}
